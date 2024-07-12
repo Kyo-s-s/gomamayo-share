f69341c5822036ae3acf0d4f431374d8319011ad
Kyo-s-s <igagrid407@gmail.com>
2024-07-12 21:55:26 +0900

    [Fix] rubocop

--- back/db/migrate/20240711053105_create_users.rb
+++ back/db/migrate/20240711053105_create_users.rb
@@ -1,10 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :password_digest
 
       t.timestamps
     end
+
+    add_index :users, :name, unique: true
   end
 end
--- back/db/schema.rb
+++ back/db/schema.rb
@@ -12,10 +12,11 @@
 
 ActiveRecord::Schema[7.1].define(version: 2024_07_11_053105) do
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
 end

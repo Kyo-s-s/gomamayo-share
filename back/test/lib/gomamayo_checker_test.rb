22b2ddddc0b82921aafad87d91996c7b52c7ff1e
Kyo-s-s <igagrid407@gmail.com>
2024-07-14 13:55:17 +0900

    [Fix]

--- back/test/lib/gomamayo_checker_test.rb
+++ back/test/lib/gomamayo_checker_test.rb
@@ -4,6 +4,8 @@ class GomamayoCheckerTest < ActiveSupport::TestCase
   include GomamayoChecker
 
   test 'mecab parse' do
-    assert mecab_parse('ホワイトとうもろこし') == "ホワイト\t名詞,一般,*,*,*,*,ホワイト,ホワイト,ホワイト\nとうもろこし\t名詞,一般,*,*,*,*,とうもろこし,トウモロコシ,トウモロコシ\nEOS\n"
+    assert mecab_parse('ホワイトとうもろこし') ==
+           "ホワイト\t名詞,一般,*,*,*,*,ホワイト,ホワイト,ホワイト\n" \
+           "とうもろこし\t名詞,一般,*,*,*,*,とうもろこし,トウモロコシ,トウモロコシ\nEOS\n"
   end
 end

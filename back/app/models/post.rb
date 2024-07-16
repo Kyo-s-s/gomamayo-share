7539010255413e3bcd302b2c58659abf1267eaa3
Kyo-s-s <igagrid407@gmail.com>
2024-07-16 14:13:10 +0900

    [Add] Like

--- back/app/models/post.rb
+++ back/app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, uniqueness: true, length: { maximum: 100 }
   validate :content_gomamayo_validation
--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_13_102149) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_050158) do
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id", null: false
@@ -29,5 +39,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_102149) do
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end

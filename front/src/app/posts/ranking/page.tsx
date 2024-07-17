fc6642e83c5d816b4c1bf8b07292b1c4e29c116a
Kyo-s-s <igagrid407@gmail.com>
2024-07-17 23:58:13 +0900

    [Add] ranking page

---
+++ front/src/app/posts/ranking/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import PostCard from "@/components/PostCard";
+import { Post, User } from "@/types/types";
+import { getRequest } from "@/utils/request";
+import { Container, Heading } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+
+type PostsApiResponse = {
+  user: User;
+  post: Post;
+  is_liked: boolean;
+}[];
+
+const PostRanking = () => {
+  const [posts, setPosts] = useState<PostsApiResponse>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getRequest<PostsApiResponse>("/posts/ranking");
+      if (res.success) {
+        setPosts(res.success);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {posts.map((post) => (
+        <PostCard key={post.post.id} {...post} />
+      ))}
+    </>
+  );
+};
+
+const Page = () => {
+  return (
+    <Container maxW="container.md">
+      <Heading>Ranking</Heading>
+      <PostRanking />
+    </Container>
+  );
+};
+
+export default Page;

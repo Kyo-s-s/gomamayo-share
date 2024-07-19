5f972e15394262d44cf28bb1a8740aa74bfe0ac9
Kyo-s-s <igagrid407@gmail.com>
2024-07-19 13:37:35 +0900

    [Fix] likes_controller

--- back/spec/factories/likes.rb
+++ back/spec/factories/likes.rb
@@ -2,15 +2,18 @@ FactoryBot.define do
   factory :like_gomamayo_kyo, class: 'Like' do
     post { association :post_gomamayo }
     user { association :user_kyo }
+    initialize_with { Like.find_or_create_by(user:, post:) }
   end
 
   factory :like_gomamayo_another, class: 'Like' do
     post { association :post_gomamayo }
     user { association :user_another }
+    initialize_with { Like.find_or_create_by(user:, post:) }
   end
 
   factory :like_asadoreretasu_kyo, class: 'Like' do
     post { association :post_asadoreretasu }
     user { association :user_kyo }
+    initialize_with { Like.find_or_create_by(user:, post:) }
   end
 end

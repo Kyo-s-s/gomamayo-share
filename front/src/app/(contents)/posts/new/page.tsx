c762193e22c735d531a84f4c1f1a94c6ff327737
Kyo-s-s <igagrid407@gmail.com>
2024-07-22 10:48:13 +0900

    [Fix] posts/new Form

--- front/src/app/(contents)/posts/new/page.tsx
+++ front/src/app/(contents)/posts/new/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { postRequest } from "@/utils/request";
 import { Post } from "@/types/types";
 import {
+  AbsoluteCenter,
   Container,
   FormControl,
   FormLabel,
@@ -12,6 +13,7 @@ import {
 } from "@chakra-ui/react";
 import { Button } from "@/components/custom";
 import useRedirect, { useRedirectIfNotLoggedIn } from "@/utils/useRedirect";
+import { Form, TextForm } from "@/components/form";
 
 const PostForm = () => {
   const redirectTo = useRedirect();
@@ -64,9 +66,51 @@ const PostForm = () => {
 
 const Page = () => {
   useRedirectIfNotLoggedIn();
+
+  const redirectTo = useRedirect();
+  const [content, setContent] = useState("");
+  const toast = useToast();
+
+  const handlePost = async () => {
+    const res = await postRequest<Post>(
+      "/posts",
+      {
+        post: { content: content },
+      },
+      true
+    );
+    if (res.success) {
+      toast({
+        title: `Success!!`,
+        position: "top",
+        duration: 1000,
+        isClosable: true,
+        status: "success",
+      });
+      redirectTo("/posts");
+    } else {
+      toast({
+        title: `Error!!`,
+        description: `${res.failure.message}`,
+        position: "top",
+        duration: 3000,
+        isClosable: true,
+        status: "error",
+      });
+    }
+  };
+
   return (
-    <Container maxW="container.md">
-      <PostForm />
+    <Container maxW="container.sm" height="90vh" position="relative">
+      <AbsoluteCenter width="100%" pb="10vh">
+        <Form title="ゴママヨ投稿" onSubmit={handlePost} submitButton="投稿">
+          <TextForm
+            value={content}
+            setValue={setContent}
+            placeholder="ごまマヨネーズ"
+          />
+        </Form>
+      </AbsoluteCenter>
     </Container>
   );
 };

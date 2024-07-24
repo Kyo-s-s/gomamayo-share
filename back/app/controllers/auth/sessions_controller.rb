e60915031a13f67606cbc14578ec6d92bf0d6b62
Kyo-s-s <igagrid407@gmail.com>
2024-07-24 15:02:57 +0900

    [Add] delete all tokens on signout

--- back/app/controllers/auth/sessions_controller.rb
+++ back/app/controllers/auth/sessions_controller.rb
@@ -1,9 +1,28 @@
-class Auth::SessionsController < ApplicationController
-  def index
-    if current_user
-      render json: { is_login: true, user: current_user }
+class Auth::SessionsController < DeviseTokenAuth::SessionsController
+  # https://github.com/lynndylanhurley/devise_token_auth/blob/master/app/controllers/devise_token_auth/sessions_controller.rb
+  def destroy
+    # remove auth instance variables so that after_action does not run
+    user = remove_instance_variable(:@resource) if @resource
+    client = @token.client
+    @token.clear!
+
+    if user && client && user.tokens[client]
+      # After sign_out, the other sessions are broken.
+      user.tokens.clear
+      # user.tokens.delete(client)
+      user.save!
+
+      if DeviseTokenAuth.cookie_enabled
+        # If a cookie is set with a domain specified then it must be deleted with that domain specified
+        # See https://api.rubyonrails.org/classes/ActionDispatch/Cookies.html
+        cookies.delete(DeviseTokenAuth.cookie_name, domain: DeviseTokenAuth.cookie_attributes[:domain])
+      end
+
+      yield user if block_given?
+
+      render_destroy_success
     else
-      render json: { is_login: false, message: 'ユーザーが存在しません' }
+      render_destroy_error
     end
   end
 end

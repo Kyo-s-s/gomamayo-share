279ca04e68509050809f810934832913c5f4546b
Kyo-s-s <86707335+Kyo-s-s@users.noreply.github.com>
2024-07-25 03:41:13 +0900

    Fix: render add entrypoint.sh (#44)

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+bundle install
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
 bundle exec rake db:migrate
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).

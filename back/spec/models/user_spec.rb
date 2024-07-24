5a5bc5b82fc40fc23424d87d0a0cc1a0812933b3
Kyo-s-s <igagrid407@gmail.com>
2024-07-24 12:11:52 +0900

    [Add] user format

--- back/spec/models/user_spec.rb
+++ back/spec/models/user_spec.rb
@@ -16,6 +16,14 @@ RSpec.describe User, type: :model do
         expect(user).to be_invalid
       end
 
+      it 'is invalid name with special characters' do
+        invalid_names = %w[!a a! a@b a-b あいうえお きょ]
+        invalid_names.each do |invalid_name|
+          user.name = invalid_name
+          expect(user).to be_invalid
+        end
+      end
+
       it 'is invalid with too long name' do
         user.name = 'a' * 21
         expect(user).to be_invalid

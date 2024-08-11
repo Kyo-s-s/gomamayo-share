e1ca96835dd37628de6eea48ed98fc2ff07bb8b5
Kyo-s-s <86707335+Kyo-s-s@users.noreply.github.com>
2024-08-11 22:14:27 +0900

    Add: #37: ogp image, analytics (#68)
    * [Add] vercel analytics
    
    * [Add] twitter-image.png

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,11 +2,16 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Gomamayo Share",
+  twitter: {
+    title: "Gomamayo Share",
+    card: "summary",
+  },
 };
 
 export default function RootLayout({
@@ -17,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <Providers>{children}</Providers>
       </body>
     </html>

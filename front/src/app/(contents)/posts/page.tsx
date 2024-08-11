dbc54fe31032159aa763920a51606b40998d1411
Kyo-s-s <86707335+Kyo-s-s@users.noreply.github.com>
2024-08-11 16:24:09 +0900

    Fix: #61: add modal for new post (#62)
    * [Add] NewPostModal
    
    * [Add] usePosts
    
    * [Fix]
    
    * [Add] loading spinner
    
    * [Add] mordal margin

--- front/src/app/(contents)/posts/page.tsx
+++ front/src/app/(contents)/posts/page.tsx
@@ -1,67 +1,30 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
-import { getRequest } from "../../../utils/request";
-import { Post, User } from "../../../types/types";
-import { Box, Container, Flex, IconButton, Link } from "@chakra-ui/react";
-import PostCard from "@/components/PostCard";
-import { useInView } from "framer-motion";
+import { useState } from "react";
+import { postRequest } from "../../../utils/request";
+import { Post } from "../../../types/types";
+import {
+  Box,
+  Container,
+  Flex,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useAuth } from "@/context/AuthContext";
 import { AddIcon } from "@chakra-ui/icons";
+import { TextForm } from "@/components/form";
+import useMessage from "@/utils/useMessage";
+import { Button } from "@/components/custom";
+import usePosts from "@/utils/usePosts";
 
-type PostsApiResponse = {
-  user: User;
-  post: Post;
-  is_liked: boolean;
-}[];
-
-const PostLoader = ({
-  posts,
-  setPosts,
-}: {
-  posts: PostsApiResponse;
-  setPosts: (post: PostsApiResponse) => void;
-}) => {
-  const [finish, setFinish] = useState(false);
-  const ref = useRef(null);
-  const isInView = useInView(ref);
-  useEffect(() => {
-    const fetchData = async () => {
-      const params: Record<string, string> = { limit: "10" };
-      if (posts.length > 0) {
-        params["timestamp"] = posts[posts.length - 1].post.created_at;
-      }
-      const res = await getRequest<PostsApiResponse>(`/posts`, params, true);
-      if (res.success) {
-        if (res.success.length === 0) {
-          setFinish(true);
-        }
-        setPosts([...posts, ...res.success]);
-      }
-    };
-
-    if (isInView) {
-      fetchData();
-    }
-  }, [isInView]);
-
-  return finish ? <></> : <Box ref={ref}>Loading...</Box>;
-};
-
-const Posts = () => {
-  const [posts, setPosts] = useState<PostsApiResponse>([]);
-
-  return (
-    <>
-      {posts.map((post) => (
-        <PostCard key={post.post.id} {...post} />
-      ))}
-      <PostLoader posts={posts} setPosts={setPosts} />
-    </>
-  );
-};
-
-const PostButton = () => {
+const PostButton = ({ onClick }: { onClick: () => void }) => {
   const { user } = useAuth();
   if (!user) {
     return <></>;
@@ -69,25 +32,81 @@ const PostButton = () => {
   return (
     <Flex justify={"flex-end"} px="20px">
       <Box position="fixed" bottom="20px">
-        <Link href="/posts/new">
-          <IconButton
-            size="lg"
-            isRound={true}
-            aria-label="New Post"
-            icon={<AddIcon />}
-          />
-        </Link>
+        <IconButton
+          onClick={onClick}
+          size="lg"
+          isRound={true}
+          aria-label="New Post"
+          icon={<AddIcon />}
+        />
       </Box>
     </Flex>
   );
 };
 
-const Page = () => {
+const NewPostModal = ({
+  isOpen,
+  onClose,
+  reload,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  reload: () => void;
+}) => {
+  const [content, setContent] = useState("");
+  const { successMessage, errorMessage } = useMessage();
+
+  const handlePost = async () => {
+    const res = await postRequest<Post>(
+      "/posts",
+      { post: { content: content } },
+      true
+    );
+    if (res.success) {
+      successMessage({});
+      onClose();
+      setContent("");
+      reload();
+    } else {
+      errorMessage({
+        description: `${res.failure.message}`,
+      });
+    }
+  };
+
   return (
-    <Container maxW="container.md" pt="50px">
-      <Posts />
-      <PostButton />
-    </Container>
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent mx={2}>
+        <ModalHeader>ゴママヨ投稿</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <TextForm
+            value={content}
+            setValue={setContent}
+            placeholder="ごまマヨネーズ"
+          />
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={handlePost}>投稿</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+const Page = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { Posts, reload } = usePosts();
+
+  return (
+    <>
+      <Container maxW="container.md" pt="50px">
+        <Posts />
+        <PostButton onClick={onOpen} />
+      </Container>
+      <NewPostModal isOpen={isOpen} onClose={onClose} reload={reload} />
+    </>
   );
 };
 

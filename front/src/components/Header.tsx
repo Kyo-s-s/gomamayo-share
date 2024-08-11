b35432e74a5ab27988174b9b6b84fad44d47bd95
Kyo-s-s <86707335+Kyo-s-s@users.noreply.github.com>
2024-08-11 20:36:30 +0900

    Fix: #60: mobile header (#64)
    * [Fix] header position sticky
    
    * [Add] MenuButtons
    
    * [Refactor]

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -8,38 +8,68 @@ import {
   Divider,
   Flex,
   Heading,
+  IconButton,
   Menu,
   MenuButton,
+  MenuDivider,
   MenuItem,
   MenuList,
   Spacer,
+  useBreakpointValue,
 } from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
+import { ChevronDownIcon, HamburgerIcon } from "@chakra-ui/icons";
 import { Link } from "@chakra-ui/next-js";
 
-
-const AccountMenuButton = () => {
+const AccountMenuItems = () => {
   const { user, logout } = useAuth();
+  return user ? (
+    <MenuItem onClick={logout}>ログアウト</MenuItem>
+  ) : (
+    <>
+      <MenuItem as="a" href="/signup">
+        アカウント登録
+      </MenuItem>
+      <MenuItem as="a" href="/login">
+        ログイン
+      </MenuItem>
+    </>
+  );
+};
+
+const MenuButtons = () => {
+  const isDesktop = useBreakpointValue({ base: false, md: true });
+
   return (
-    <Menu>
-      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-        Account
-      </MenuButton>
-      <MenuList>
-        {user ? (
-          <MenuItem onClick={logout}>Logout</MenuItem>
-        ) : (
-          <>
-            <MenuItem as="a" href="/signup">
-              アカウント登録
+    <Flex gap={2}>
+      {isDesktop ? (
+        <>
+          <LinkButton href={`/posts`}>タイムライン</LinkButton>
+          <LinkButton href={`/posts/ranking`}>ランキング</LinkButton>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              アカウント
+            </MenuButton>
+            <MenuList>
+              <AccountMenuItems />
+            </MenuList>
+          </Menu>
+        </>
+      ) : (
+        <Menu>
+          <MenuButton as={IconButton} icon={<HamburgerIcon />} />
+          <MenuList>
+            <MenuItem as="a" href="/posts">
+              タイムライン
             </MenuItem>
-            <MenuItem as="a" href="/login">
-              ログイン
+            <MenuItem as="a" href="/posts/ranking">
+              ランキング
             </MenuItem>
-          </>
-        )}
-      </MenuList>
-    </Menu>
+            <MenuDivider />
+            <AccountMenuItems />
+          </MenuList>
+        </Menu>
+      )}
+    </Flex>
   );
 };
 
@@ -47,7 +77,7 @@ const Header = () => {
   return (
     <header
       style={{
-        position: "fixed",
+        position: "sticky",
         top: "0pt",
         left: "0pt",
         width: "100%",
@@ -55,17 +85,13 @@ const Header = () => {
         zIndex: 2,
       }}
     >
-      <Container maxW="container.lg">
+      <Container maxW="container.lg" p={2}>
         <Flex>
           <Link href="/">
             <Heading as="h2">Gomamayo Share</Heading>
           </Link>
           <Spacer />
-          <Flex gap={2}>
-            <LinkButton href={`/posts`}>Posts</LinkButton>
-            <LinkButton href={`/posts/ranking`}>Ranking</LinkButton>
-            <AccountMenuButton />
-          </Flex>
+          <MenuButtons />
         </Flex>
       </Container>
       <Divider />

6397700d0631e658bee7e736055eb04228567c53
Kyo-s-s <igagrid407@gmail.com>
2024-08-12 16:05:46 +0900

    [Fix] posts/id page

--- front/src/app/(contents)/posts/[id]/page.tsx
+++ front/src/app/(contents)/posts/[id]/page.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import Background from "@/components/Background";
+import { LinkText } from "@/components/custom";
 import PostCard from "@/components/PostCard";
 import { Post, User } from "@/types/types";
 import { getRequest } from "@/utils/request";
-import { Heading } from "@chakra-ui/react";
+import {
+  AbsoluteCenter,
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Spacer,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 type PostApiResponse = {
@@ -26,14 +36,34 @@ const PostPage = ({ id }: { id: string }) => {
     };
     if (post == null) fetchData();
   }, [post]);
-  return <>{post ? <PostCard {...post} /> : <p>Loading...</p>}</>;
+  return (
+    <>
+      {post ? (
+        <PostCard {...post} />
+      ) : (
+        // FIXME: Spinner.tsx に切り出す
+        <Box textAlign="center">
+          <Spinner thickness="5px" color="gray.400" speed="0.75s" size="xl" />
+        </Box>
+      )}
+    </>
+  );
 };
 const Page = ({ params }: { params: { id: string } }) => {
   return (
     <>
       <Background />
-      <Heading>posts {params.id}</Heading>
-      <PostPage id={params.id} />
+      <Container maxW="container.md" height="90svh" position="relative">
+        <AbsoluteCenter width="100%" px={4}>
+          <PostPage id={params.id} />
+          <Flex>
+            <Spacer />
+            <Box bg="rgba(255, 255, 255, 0.8)" px={2} borderRadius={4}>
+              <LinkText href="/posts">タイムラインへ</LinkText>
+            </Box>
+          </Flex>
+        </AbsoluteCenter>
+      </Container>
     </>
   );
 };

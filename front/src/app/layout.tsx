8bb959c0d2f76c3c0f5a63099bed60a913360817
Kyo-s-s <igagrid407@gmail.com>
2024-08-17 14:38:15 +0900

    [Add] zen maru gothic

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Analytics } from "@vercel/analytics/react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Gomamayo Share",
@@ -21,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Analytics />
         <Providers>{children}</Providers>
       </body>
--- front/src/app/providers.tsx
+++ front/src/app/providers.tsx
@@ -1,14 +1,32 @@
 "use client";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
+import { Zen_Maru_Gothic } from "next/font/google";
 const AuthProvider = dynamic(() => import("../context/AuthContext"), {
   ssr: false,
 });
 
+const zenMaruGothic = Zen_Maru_Gothic({
+  subsets: ["latin"],
+  weight: "500",
+});
+
+const zenMaruGothicBold = Zen_Maru_Gothic({
+  subsets: ["latin"],
+  weight: "700",
+});
+
+const theme = extendTheme({
+  fonts: {
+    body: zenMaruGothic.style.fontFamily,
+    heading: zenMaruGothicBold.style.fontFamily,
+  },
+});
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <AuthProvider>{children}</AuthProvider>
     </ChakraProvider>
   );
